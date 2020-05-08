867ca26f8ec0eaac06e8ed1ceb170a4127508077
Sasha <kazhar1@stolaf.edu>
2020-05-07 19:17:59 -0500

    a part of the final

---
+++ finalproject/my_ass.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Homescreen {
+    char *day;
+};
+
+class WorkingScreen {
+    char *d;
+
+
+}
+
+;
+int main() {
+    init();
+
+    const int index 1 = 1 - ;
+    if (just_starting()) {
+        print_at(index_1, "");
+    }
+    add_yaml("homescreen_buttons.yaml", {{"index", index_1}});
+    quit();
+}
---
+++ hw11/simple_button.cpp
@@ -0,0 +1,7 @@
+#include"../react.h"
+  
+int main() {
+  init();
+  add_yaml("app.yaml");
+  quit();
+}
