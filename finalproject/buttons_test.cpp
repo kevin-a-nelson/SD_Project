213ff1381a9cef3285b0acd08a49b1ac9e6e9363
Sasha <kazhar1@stolaf.edu>
2020-05-11 20:14:32 -0500

    react.o

--- finalproject/buttons_test.cpp
+++ finalproject/buttons_test.cpp
@@ -3,31 +3,22 @@
 int main() {
   init();
   char state = get_char_at(2);
-  print_at(5, "hello world!");
-  add_yaml("text.yaml");
-  const int index_1 = 10;
-    if (just_starting()) {
-      print_at(index_1, "");
-  }
-
-      add_yaml("input.yaml", {{"index", index_1}});
-
-  print_at(5, "hello");
   if (just_starting()) {
-    state = 'H';
+    state = 'H';  
     put_char_at(2, state);
   } else if (received_event()) {
     if (event_id_is("to_screen_two")) {
       state = 'T';
       put_char_at(2, state);
     } else if (event_id_is("button_1")) {
-      cerr << "ok!";
+      cerr << "ok!" << endl;
     } else if (event_id_is("to_home_screen")) {
-      state = 'H';
+      state = 'h';
       put_char_at(2, state);
     }
   }
-  if ('H' == state) add_yaml("home.yaml");
-  else add_yaml("screen1.yaml");
+  if ('H' == state) add_yaml("buttons_test.yaml");
+  else add_yaml("buttns_screen1_test.yaml");
   quit();
 }
+
